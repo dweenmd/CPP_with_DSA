b575152b171d923c9abc9b6c851dbb063f782a62
Dween Mohammad <dween.md01@gmail.com>
2024-02-14 12:43:27 +0600

    tamplate_base_stack_usingSinglyLinkedList

--- stack/tamplate_base_stack_usingSinglyLinkedList.cpp
+++ stack/tamplate_base_stack_usingSinglyLinkedList.cpp
@@ -6,109 +6,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<class T>
 class node
 {
 public:
-    int data;
+    T data;
     node *next;
 };
-
+template<class T>
 class LinkedList
 {
 public:
-    node *head;
+    node<T> *head;
     int sz = 0; // for linklist size
     LinkedList()
     {
         head = NULL; // initiali head ta null e point kore thakbe
+        sz=0;
     }
     // creats a new node with data= value and nxt = NULL
-    node *CreateNewNode(int value)
+    node<T> *CreateNewNode(T value)
     {
-        node *newnode = new node;
+        node<T> *newnode = new node<T>;
         newnode->data = value;
         newnode->next = NULL;
         return newnode;
     }
     // insert new value at head
-    void InsertAtHead(int value) // Added the 'value' parameter
+    void InsertAtHead(T value) // Added the 'value' parameter
     {
         sz++; // kono kichu insert korle linklist er size increase hobe
-        node *a = CreateNewNode(value);
+        node<T> *a = CreateNewNode(value);
         if (head == NULL)
         {
             head = a;
             return;
         }
+        
         a->next = head; // Fixed the typo here
         head = a;
     }
     // prints the linkedlist
     void Traverse()
     {
-        node *a = head;
+        node<T> *a = head;
         while (a != NULL)
         {
             cout << a->data << " ";
             a = a->next; // Fixed the typo here
         }
         cout << "\n";
-    }
-
-    // search for a single value
-    int SearchDistinctValue(int value)
-    {
-        node *a = head;
-        int index = 0;
-        while (a != NULL)
-        {
-            if (a->data == value)
-            {
-                return index;
-            }
-            a = a->next;
-            index++;
-        }
-        return -1;
-    }
-    // search all possible occurence
-    void SearchAllValue(int value)
-    {
-        node *a = head;
-        int index = 0;
-        while (a != NULL)
-        {
-            if (a->data == value)
-            {
-                cout << value << " is found at index " << index << endl;
-            }
-            a = a->next;
-            index++;
-        }
-    }
-    // add any element at any index
-    void InsertAtAnyIndex(int index, int value)
-    {
-        if (index < 0 || index > sz) // corner case
-        {
-            return;
-        }
-        if (index == 0)
-        {
-            InsertAtHead(value);
-            return;
-        }
-        sz++;
-        node *a = head;
-        int currentIndex = 0;
-        while (currentIndex != index - 1)
-        {
-            a = a->next;
-            currentIndex++;
-        }
-        node *newnode = CreateNewNode(value);
-        newnode->next = a->next;
-        a->next = newnode;
     }
     // first index ba head ke delete kora
     void DeleteAtHead()
@@ -118,160 +65,93 @@ public:
             return;
         }
         sz--;
-        node *a = head;
+        node<T> *a = head;
         head = a->next;
         delete a;
     }
-    // delet any index
-    void DeleteAnyIndex(int index)
-    {
-        if (index < 0 || index > sz - 1)
-        {
-            return;
-        }
-        if (index == 0)
-        {
-            DeleteAtHead();
-            return;
-        }
-        sz--;
-        node *a = head;
-        int currentIndex = 0;
-        while (currentIndex != index - 1)
-        {
-            a = a->next;
-            currentIndex++;
-        }
-        node *b = a->next;
-        a->next = b->next;
-        delete b;
-    }
-    void InsertAfterValue(int value, int data)
-    {
-        node *a = head;
-        while (a != NULL)
-        {
-            if (a->data == value)
-            {
-                break;
-            }
-            a = a->next;
-        }
-        if (a == NULL)
-        {
-            cout << value << " doesn't exist in linked-list" << endl;
-            return;
-        }
-        sz++;
-        node *newnode = CreateNewNode(data);
-        newnode->next = a->next;
-        a->next = newnode;
-    }
-    // pritn the reverse order from node a to last
-    void ReversePrint2(node *a)
-    {
-        if (a == NULL)
-        {
-            return;
-        }
-        ReversePrint2(a->next);
-        cout << a->data << " ";
-    }
-    void ReversePrint()
-    {
-        ReversePrint2(head);
-        cout << "\n";
-    }
-
-    /*
-    int getSize() //time complexity => O(n)
-    {
-        int sz = 0;
-        node *a = head;
-        while (a != NULL)
-        {
-            sz++;
-            a = a->next;
-        }
-        return sz;
-    }
-    */
-
     // this is run in constant order. => O(1)
     int getSize()
     {
         return sz;
     }
 };
+template<class T>
+class Stack
+{
+    public:
+    LinkedList<T>l;
+    Stack()
+    {
+
+    }
+    T top()
+    {
+        if(l.getSize()==0)
+        {
+            cout<<"Stack is Empty!";
+            T n;
+            return n;
+        }
+        return l.head->data;
+    }
+    void push(T val)
+    {
+        l.InsertAtHead(val);
+    }
+    void pop()
+    {
+        if(l.getSize()==0)
+        {
+            cout<<"Stack is empty!"<<endl;
+            return;
+        }
+        l.DeleteAtHead();
+    }
+};
 
 int main()
 {
-    LinkedList l;
-    l.InsertAtHead(10);
-    l.InsertAtHead(20);
-    l.InsertAtHead(25);
-    l.InsertAtHead(30);
-    l.InsertAtHead(40);
-    l.InsertAtHead(30);
+    Stack<int> st;
+    st.push(3);
+    cout << st.top() << "\n";
+    st.push(4);
+    cout << st.top() << "\n";
+    st.push(5);
+    cout << st.top() << "\n";
 
-    l.Traverse();
+    st.pop();
+    cout << st.top() << "\n";
+    st.pop();
+    cout << st.top() << "\n";
+    st.pop();
+    cout << st.top() << "\n";
 
-    cout << "5 is found at " << l.SearchDistinctValue(5) << endl;
-    cout << "10 is found at " << l.SearchDistinctValue(10) << endl;
+    Stack<char> st1;
+    st1.push('a');
+    st1.push('b');
+    st1.push('c');
+    cout << st1.top() << "\n";
+    st1.pop();
+    cout << st1.top() << "\n";
+    st1.pop();
+    cout << st1.top() << "\n";
+    st1.pop();
+    cout << st1.top() << "\n";
 
-    l.SearchAllValue(30);
-    cout << "size of linklist: ";
-    cout << l.getSize() << endl;
-    cout << '\n'
-         << "insert at any index: (4)." << endl;
 
-    l.InsertAtAnyIndex(4, 675);
+  Stack<float> st2;
+    st2.push(3.56);
+    cout << st2.top() << "\n";
+    st2.push(4.67);
+    cout << st2.top() << "\n";
+    st2.push(5.78);
+    cout << st2.top() << "\n";
 
-    l.Traverse();
-    cout << "------------------------------------------" << endl;
-    cout << "delete head index:" << endl;
-    l.DeleteAtHead();
-    l.Traverse();
-    cout << "size of linklist: " << l.getSize() << endl;
-    cout << "Delete any index:(2)" << endl;
-    l.DeleteAnyIndex(2);
-    l.Traverse();
-    cout << "size of linklist: " << l.getSize() << endl;
-    cout << "------------------------------------------" << endl;
-    cout << "insert after value:(20) " << endl;
-    l.InsertAfterValue(20, 420);
-    l.Traverse();
-    cout << "size of linklist: " << l.getSize() << endl;
-    cout << "------------------------------------------" << endl;
-    cout << " reverse order print: ";
-    l.ReversePrint();
-    
-    return 0; // Added a return statement
+    st2.pop();
+    cout << st2.top() << "\n";
+    st2.pop();
+    cout << st2.top() << "\n";
+    st2.pop();
+    cout << st2.top() << "\n";
+    return 0;
 }
-/*
-OUTPUT
-
-30 40 30 25 20 10 
-5 is found at -1
-10 is found at 5
-30 is found at index 0
-30 is found at index 2
-size of linklist: 6
-
-insert at any index: (4).
-30 40 30 25 675 20 10 
-------------------------------------------
-delete head index:
-40 30 25 675 20 10 
-size of linklist: 6
-Delete any index:(2)
-40 30 675 20 10 
-size of linklist: 5
-------------------------------------------
-insert after value:(20) 
-40 30 675 20 420 10 
-size of linklist: 6
-------------------------------------------
- reverse order print: 10 420 20 675 30 40
-
-*/
