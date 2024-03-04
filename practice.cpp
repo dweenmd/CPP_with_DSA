5c44eec82bf8e719d52c8d61b6cf6fd2a5b3d8f9
Dween Mohammad <dween.md01@gmail.com>
2024-03-04 22:30:30 +0600

    linkedlist

--- practice.cpp
+++ practice.cpp
@@ -1,51 +1,118 @@
-/**!
-Write a C++ program to find the intersection of two arrays using a set.
-Input
-
-5
-1 2 3 4 5
-6
-3 4 5 6 7 9
-Output
-3 4 5
-
-The first array is [1,2,3,4,5] and the second array is [3,4,5,6,7,9]. Their intersection is [3,4,5]
-
-!**/
-#include <bits/stdc++.h>
-#define ll long long int
-#define asc(v) sort(v.begin(), v.end()) // vectors,pairs,tuples,string
-#define dsc(v) sort(v.begin(), v.end(), greater<int>())
-
+#include <iostream>
 using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+class DoublyLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int size;
+
+public:
+    DoublyLinkedList()
+    {
+        head = NULL;
+        tail = NULL;
+        size = 0;
+    }
+
+    void insertHead(int value)
+    {
+        Node *newNode = new Node(value);
+        if (!head)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    void insertTail(int value)
+    {
+        Node *newNode = new Node(value);
+        if (!tail)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void insertMid(int value)
+    {
+        Node *newNode = new Node(value);
+        if (!head)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            Node *slow = head;
+            Node *fast = head->next;
+            while (fast && fast->next)
+            {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            newNode->prev = slow;
+            newNode->next = slow->next;
+            if (slow->next)
+                slow->next->prev = newNode;
+            slow->next = newNode;
+            if (!fast) // if the list has odd number of nodes
+                tail = newNode;
+        }
+        size++;
+    }
+
+    void display()
+    {
+        Node *current = head;
+        while (current)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n;
-    cin >> n;
-    set<int> st1;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        st1.insert(x);
-    }
-    int m;
-    cin >> m;
-    set<int> st2;
-    for (int i = 0; i < m; i++)
-    {
-        int x;
-        cin >> x;
-        st2.insert(x);
-    }
-    for (auto it : st1)
-    {
-        if (st2.find(it) != st2.end())
-            cout << it << " ";
-    }
-
+    DoublyLinkedList dll;
+    dll.insertHead(10);
+    dll.insertHead(20);
+    dll.insertTail(30);
+    dll.insertTail(40);
+    dll.insertMid(25);
+    dll.display(); // Output: 20 10 25 30 40
     return 0;
 }
