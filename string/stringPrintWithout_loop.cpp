5abd05f9a37afa99c19d183efa86558afe88c753
Dween Mohammad <dween.md01@gmail.com>
2024-11-12 20:46:58 +0600

    update

---
+++ string/stringPrintWithout_loop.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+  int n;cin>>n;
+    cout<<string(n-1,'2')<<"6"<<'\n';
+    cout<<string(n-1,'3')<<"9"<<'\n';
+}
+
